c1b5dd700c08da4a460344933e46145170aa320b
Ross Golder <ross@golder.org>
2025-10-28 15:28:57 +0700

    fix: update dependencies and fix kingpin import path
    - Update import from gopkg.in/alecthomas/kingpin.v2 to github.com/alecthomas/kingpin/v2
    - Run go get -u and go get -u ./... to update all dependencies
    - Resolve module path conflicts for successful dependency updates

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -22,7 +22,7 @@ import (
 	"runtime"
 	"time"
 
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
+	kingpin "github.com/alecthomas/kingpin/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
